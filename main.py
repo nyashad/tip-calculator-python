104061953e7a7e10be122086cd70a1415325b1f9
Nyasha <nynyashadzashe@gmail.com>
2024-08-10 01:40:27 +0200

    updated the input values

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? R"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+bill = float(input("What is the total bill? R:"))
+tip = int(input("What percentage tip would you like to give? (Please just write the number, it automatically adds the percentage) 10 12 15 or any of your choice "))
 people = int(input("How many people to split the bill? "))
 tip_as_percentage = tip/100
 tip_amount=bill*tip_as_percentage
