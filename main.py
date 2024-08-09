0d3d2c8690f4890bda9c82a8dd2e00568c5a7c97
Nyasha <nynyashadzashe@gmail.com>
2024-08-10 01:26:01 +0200

    created formula

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? R"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+tip_as_percentage = tip/100
+tip_amount=bill*tip_as_percentage
+total_bill=bill+tip_amount
+bill_per_person=total_bill/people
+print(round(bill_per_person, 2))
